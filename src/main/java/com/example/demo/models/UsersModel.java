6b5f301c140b2e18fdf1671350848ab444741a54
moman <momaniammar95@gmail.com>
2025-07-10 18:16:45 +0300

    created the model for the users table with getters for all but only setters for what we can change

---
+++ src/main/java/com/example/demo/models/UsersModel.java
@@ -0,0 +1,42 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+public class UsersModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userid;
+
+    @Column(unique = true, nullable = false)
+    @Email(message = "not a valid e-mail address")
+    @NotBlank(message = "e-mail cannot be empty")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "password cannot be empty")
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    @CreationTimestamp
+    private LocalDateTime creationtime;
+
+    public Long getUserid() { return userid; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getUsername() { return username; }
+    public LocalDateTime getCreationtime() { return creationtime; }
+
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setUsername(String username) { this.username = username; }
+
+}
