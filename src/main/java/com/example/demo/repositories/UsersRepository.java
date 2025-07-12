2b2f84f7b153dc7da21fd3b2cd3740bf3e64d6a7
moman <momaniammar95@gmail.com>
2025-07-12 17:34:32 +0300

    create a temporary repository for users

---
+++ src/main/java/com/example/demo/repositories/UsersRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Users;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByUserid(Long userid);
+    Optional<Users> findByEmail(String email);
+    Optional<Users> findByUsername(String username);
+
+}
