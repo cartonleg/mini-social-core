cb811a67fac6b9a2b4701d549f1295ecd4d01cbe
moman <momaniammar95@gmail.com>
2025-07-12 20:14:29 +0300

    created the end-point to register(still not done)

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controllers;
 
+import com.example.demo.DTO.UsersRequestDTO;
+import com.example.demo.models.Users;
 import com.example.demo.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ public class UsersController {
     @Autowired
     public UsersController(UsersService usersService) {
         this.usersService = usersService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Users> registerUser(@RequestBody UsersRequestDTO requestDTO) {
+        Users user =usersService.registerUser(requestDTO);
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/example/demo/services/UsersService.java
+++ src/main/java/com/example/demo/services/UsersService.java
@@ -1,6 +1,9 @@
 package com.example.demo.services;
 
+import com.example.demo.DTO.UsersRequestDTO;
+import com.example.demo.models.Users;
 import com.example.demo.repositories.UsersRepository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +11,17 @@ import org.springframework.stereotype.Service;
 public class UsersService {
     private final UsersRepository usersRepository;
 
-    @Autowired UsersService(UsersRepository usersRepository) {
+    @Autowired
+    UsersService(UsersRepository usersRepository) {
         this.usersRepository = usersRepository;
     }
+
+    public Users registerUser(UsersRequestDTO requestDTO) {
+        Users user = new Users();
+        user.setEmail(requestDTO.getEmail());
+        user.setPassword(requestDTO.getPassword());
+        user.setUsername(requestDTO.getUsername());
+        return usersRepository.save(user);
+    }
+
 }
