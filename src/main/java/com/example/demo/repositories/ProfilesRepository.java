056d5cd37af0661a2a654fad2264cac8d432e8eb
moman <momaniammar95@gmail.com>
2025-07-13 22:52:30 +0300

    created the profiles repository with find by profile id and user id

---
+++ src/main/java/com/example/demo/repositories/ProfilesRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Profiles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfilesRepository extends JpaRepository<Profiles, Long> {
+    Optional<Profiles> findByProfileid(Long profileid);
+    Optional<Profiles> findByUserid(Long userid);
+}
