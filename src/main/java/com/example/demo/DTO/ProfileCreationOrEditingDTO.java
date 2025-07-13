747fb5697693f734ea7a88fbdecae73e400e4f37
moman <momaniammar95@gmail.com>
2025-07-13 22:53:54 +0300

    created the dto for profile creation or editing dto

---
+++ src/main/java/com/example/demo/DTO/ProfileCreationOrEditingDTO.java
@@ -0,0 +1,20 @@
+package com.example.demo.DTO;
+
+import jakarta.validation.constraints.Size;
+
+public class ProfileCreationOrEditingDTO {
+    @Size(max = 50, message = "Bio must me a maximum of 50 characters.")
+    private String bio;
+
+    @Size(max = 10, message = "Display name must be a maximum of 10 characters.")
+    private String displayname;
+
+    private Long userid;
+
+    public String getBio() { return this.bio; }
+    public String getDisplayname() { return this.displayname; }
+    public Long getUserid() { return this.userid; }
+
+    public void setBio(String bio) { this.bio = bio; }
+    public void setDisplayname(String displayname) { this.displayname = displayname; }
+}
