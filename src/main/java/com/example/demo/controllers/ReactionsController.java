502b54ad3f29cb3ec9aa46ce976cc5e8cd66205f
moman <momaniammar95@gmail.com>
2025-07-17 19:53:54 +0300

    the logic for reacting to a post

--- src/main/java/com/example/demo/controllers/ReactionsController.java
+++ src/main/java/com/example/demo/controllers/ReactionsController.java
@@ -1,15 +1,27 @@
 package com.example.demo.controllers;
 
+import com.example.demo.DTO.ReactionsDTO;
+import com.example.demo.DTO.ReactionsResponseDTO;
 import com.example.demo.services.ReactionsService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/reactions")
 public class ReactionsController {
     private ReactionsService reactionsService;
 
     @Autowired
     public ReactionsController(ReactionsService reactionsService){
         this.reactionsService = reactionsService;
+    }
+
+    @PostMapping("/create/{id}")
+    public ResponseEntity<?> reactToPost(@Valid @RequestBody ReactionsDTO requestDTO, @PathVariable Long id){
+        ReactionsDTO response = reactionsService.reactToPost(requestDTO, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/example/demo/services/ReactionsService.java
+++ src/main/java/com/example/demo/services/ReactionsService.java
@@ -1,16 +1,42 @@
 package com.example.demo.services;
 
+import com.example.demo.DTO.ReactionsMapper;
+import com.example.demo.DTO.ReactionsDTO;
+import com.example.demo.DTO.ReactionsResponseDTO;
+import com.example.demo.exceptions.DoesNotExistException;
+import com.example.demo.models.Posts;
+import com.example.demo.models.Reactions;
+import com.example.demo.models.Users;
+import com.example.demo.repositories.PostsRepository;
 import com.example.demo.repositories.ReactionsRepository;
+import com.example.demo.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReactionsService {
+    private final ReactionsMapper reactionsMapper;
+    private final PostsRepository postsRepository;
+    private final UsersRepository usersRepository;
     private ReactionsRepository reactionsRepository;
 
     @Autowired
-    public ReactionsService(ReactionsRepository reactionsRepository){
+    public ReactionsService(ReactionsRepository reactionsRepository, ReactionsMapper reactionsMapper, PostsRepository postsRepository, UsersRepository usersRepository){
         this.reactionsRepository = reactionsRepository;
+        this.reactionsMapper = reactionsMapper;
+        this.postsRepository = postsRepository;
+        this.usersRepository = usersRepository;
     }
 
+    public ReactionsDTO reactToPost(ReactionsDTO requestDTO, Long id){
+        requestDTO.setUserid(id);
+        Posts post = postsRepository.findByPostid(requestDTO.getPostid()).orElse(null);
+        Users user = usersRepository.findByUserid(requestDTO.getUserid()).orElse(null);
+        if (post == null || user == null){
+            throw new DoesNotExistException("Either user or post does not exist.");
+        }
+        Reactions reaction = reactionsMapper.toReaction(requestDTO);
+        reactionsRepository.save(reaction);
+        return reactionsMapper.toReactionDTO(reaction);
+    }
 }
