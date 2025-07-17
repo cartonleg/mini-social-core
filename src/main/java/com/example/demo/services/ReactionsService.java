a0f3af2047cbc5bce674f117eef0532814580e91
moman <momaniammar95@gmail.com>
2025-07-17 20:31:37 +0300

    added to check if the person who reacts already created a profile or not

--- src/main/java/com/example/demo/services/ReactionsService.java
+++ src/main/java/com/example/demo/services/ReactionsService.java
@@ -3,11 +3,15 @@ package com.example.demo.services;
 import com.example.demo.DTO.ReactionsMapper;
 import com.example.demo.DTO.ReactionsDTO;
 import com.example.demo.DTO.ReactionsResponseDTO;
+import com.example.demo.enums.ReactionsEnums;
 import com.example.demo.exceptions.DoesNotExistException;
+import com.example.demo.exceptions.InvalidInputException;
 import com.example.demo.models.Posts;
+import com.example.demo.models.Profiles;
 import com.example.demo.models.Reactions;
 import com.example.demo.models.Users;
 import com.example.demo.repositories.PostsRepository;
+import com.example.demo.repositories.ProfilesRepository;
 import com.example.demo.repositories.ReactionsRepository;
 import com.example.demo.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,25 +22,29 @@ public class ReactionsService {
     private final ReactionsMapper reactionsMapper;
     private final PostsRepository postsRepository;
     private final UsersRepository usersRepository;
+    private final ProfilesRepository profilesRepository;
     private ReactionsRepository reactionsRepository;
 
     @Autowired
-    public ReactionsService(ReactionsRepository reactionsRepository, ReactionsMapper reactionsMapper, PostsRepository postsRepository, UsersRepository usersRepository){
+    public ReactionsService(ReactionsRepository reactionsRepository, ReactionsMapper reactionsMapper, PostsRepository postsRepository, UsersRepository usersRepository, ProfilesRepository profilesRepository){
         this.reactionsRepository = reactionsRepository;
         this.reactionsMapper = reactionsMapper;
         this.postsRepository = postsRepository;
         this.usersRepository = usersRepository;
+        this.profilesRepository = profilesRepository;
     }
 
     public ReactionsDTO reactToPost(ReactionsDTO requestDTO, Long id){
         requestDTO.setUserid(id);
         Posts post = postsRepository.findByPostid(requestDTO.getPostid()).orElse(null);
+        Profiles profile = profilesRepository.findByUsers_Userid(requestDTO.getUserid()).orElse(null);
         Users user = usersRepository.findByUserid(requestDTO.getUserid()).orElse(null);
-        if (post == null || user == null){
-            throw new DoesNotExistException("Either user or post does not exist.");
+        if (post == null || profile == null){
+            throw new DoesNotExistException("Either user or post does not exist, or the user trying to react didn't create a profile yet.");
         }
         Reactions reaction = reactionsMapper.toReaction(requestDTO);
         reactionsRepository.save(reaction);
         return reactionsMapper.toReactionDTO(reaction);
     }
+
 }
