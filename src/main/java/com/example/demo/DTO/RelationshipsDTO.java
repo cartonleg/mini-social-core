58df462b273c8c919d3d316db9a8d36b5208e2cb
moman <momaniammar95@gmail.com>
2025-07-18 21:34:39 +0300

    created the dto for relationships, for now we will only need one, and since will only map it in 1 or 2 functions, then I'm not creating a mapper for now to avoid wasting time since I'm nearing a deadline

---
+++ src/main/java/com/example/demo/DTO/RelationshipsDTO.java
@@ -0,0 +1,18 @@
+package com.example.demo.DTO;
+
+import com.example.demo.enums.RelationshipsEnums;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class RelationshipsDTO {
+    @NotNull(message = "Please specify who you want to send a friend request to by id.")
+    private Long receiverId;
+
+    private RelationshipsEnums status;
+
+    public Long getReceiverId() { return this.receiverId; }
+    public RelationshipsEnums getStatus() { return this.status; }
+
+    public void setReceiverId(Long receiverUsername) { this.receiverId = receiverUsername; }
+    public void setStatus(RelationshipsEnums status) { this.status = status; }
+}
